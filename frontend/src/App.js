421527244ccc0e53fc269161d717b3999938575f
XSeann <seannejmalimpenet@gmail.com>
2023-09-02 16:34:17 -0700

    update 1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-//import { useState } from "react"
+import { useState } from "react"
 
 import { io } from 'socket.io-client'
 
 const App = () => {
-
+  const [message, setMessage] = useState([])
   const send = () => {
     const socket = io();
 
     // send a message to the server
-    socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
+    socket.emit("chat message", 5, "6", { 7: Uint8Array.from([8]) });
 
     // receive a message from the server
-    socket.on("hello from server", (...args) => {
+    socket.on("chat message", (...args) => {
+      setMessage(e => [...e, args])
       console.log(args)
     });
   }
 
   return (
     <div className="App">
+      {message.length && message.map(data => 
+        <div>
+          {data}
+        </div>
+      )}
       <button onClick={send}>SEND</button>
     </div>
   )
