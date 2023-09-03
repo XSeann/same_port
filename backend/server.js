4a708c8c12a5fe5edab187cec6986deb26761704
XSeann <seannejmalimpenet@gmail.com>
2023-09-03 10:27:03 -0700

    try

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ io.on('connection', function(socket) {
 
   socket.on('chat message', function(msg) {
     console.log("Received a chat message");
+    console.log(msg)
     io.emit('chat message', msg);
   });
 })
